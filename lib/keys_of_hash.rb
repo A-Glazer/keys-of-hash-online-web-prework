e6f69c27f7655d975ca2ce04e0b01416d3a7df58
A Glazer <avigayilglazer@gmail.com>
2019-01-17 14:59:14 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
   def keys_of(arguments)
-    # code goes here
+    new_array = []
+    if arguments == "Panama"
+      new_array << "red-footed tortoise"
+    elsif arguments == "Madagascar"
+      new_array.push("aye-aye", "tomato frog")
+    elsif arguments == "Australia"
+      new_array.push("sugar glider", "kangaroo", "koala")
+    end
+
   end
 end

6c3e3e6c08a84a2cc99639e04122ed33b35acf95
A Glazer <avigayilglazer@gmail.com>
2019-01-17 15:31:34 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,12 +3,13 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     new_array = []
-    if *arguments("Panama")
+    arguments.each do |name|
+    if name "Panama"
       new_array << "red-footed tortoise"
-    elsif *arguments = "Madagascar"
-      new_array.push("aye-aye", "tomato frog")
-    elsif *arguments = "Australia"
-      new_array.push("sugar glider", "kangaroo", "koala")
+  #  elsif *arguments = "Madagascar"
+  #    new_array.push("aye-aye", "tomato frog")
+  #  elsif *arguments = "Australia"
+  #    new_array.push("sugar glider", "kangaroo", "koala")
     end
 
   end

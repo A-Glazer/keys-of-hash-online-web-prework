22cb7d8fa570f5a34899f8e8fcefcc9d5a03c806
A Glazer <avigayilglazer@gmail.com>
2019-01-17 15:32:57 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -6,11 +6,11 @@ class Hash
     arguments.each do |name|
     if name == "Panama"
       new_array << "red-footed tortoise"
-    
-    elsif *arguments = "Madagascar"
-  #    new_array.push("aye-aye", "tomato frog")
-  #  elsif *arguments = "Australia"
-  #    new_array.push("sugar glider", "kangaroo", "koala")
+
+    elsif name == "Madagascar"
+      new_array.push("aye-aye", "tomato frog")
+    elsif name == "Australia"
+      new_array.push("sugar glider", "kangaroo", "koala")
     end
     end
   end

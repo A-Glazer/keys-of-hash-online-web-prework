dca45de0423ce8e9202a8ccc242550a2283aacbf
A Glazer <avigayilglazer@gmail.com>
2019-01-17 15:29:28 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     new_array = []
-    if *arguments = "Panama"
+    if *arguments("Panama")
       new_array << "red-footed tortoise"
     elsif *arguments = "Madagascar"
       new_array.push("aye-aye", "tomato frog")

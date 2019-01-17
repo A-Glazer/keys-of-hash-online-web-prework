823447e50bb95e7be88bc1a4ed33f8026710465b
A Glazer <avigayilglazer@gmail.com>
2019-01-17 16:13:20 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,15 +1,12 @@
 require 'pry'
 
 class Hash
-  def keys_of(arguments)
-    new_array = []
-    if arguments == "Panama"
-      new_array << "red-footed tortoise"
-    elsif arguments == "Madagascar"
-      new_array.push("aye-aye", "tomato frog")
-    elsif arguments == "Australia"
-      new_array.push("sugar glider", "kangaroo", "koala")
-    end
-binding.pry
+  def keys_of(*arguments)
+
+    self.map {|key, value| arguments.include?(value) ? key : nil }.compact
+
+
+
+
   end
 end
